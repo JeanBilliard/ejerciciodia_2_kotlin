d64dc3ac3dbf71b7707fed578d0a67de6c1ba941
jeanbilliard <jpbilliard@gmail.com>
2021-01-28 18:09:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ejerciciodia_2_kotlin/ejercicio_2_libros_kotlin.kt
@@ -0,0 +1,28 @@
+package com.example.ejerciciodia_2_kotlin
+
+fun main(){
+    println("El Libro: $balancin")
+    println(caligrafics("El precio: $caligrafics"))
+    
+    var balancin = listOf("Balancin","Año 2020","Paginas: 112", "Autor: Caligrafics",'I')
+    var PapeluchoH = listOf("Papelucho Historiador","Año: 1955 ","Paginas: 46","Autor: Marcela Paz",
+        'D')
+    var ami = listOf ("Ami , El niño de las estrellas", "Año: 1986","Paginas: 151",
+        "Autor: Enrique Barrios", 'I')
+    val libro = listOf("Super Mente", "año 2014", "Paginas: 47", "Autor: Larousse", 'I')
+}
+
+fun precioformateado(){
+    var caligrafics:Int=6290
+    var marcela:Int=5500
+        var enrique:Int=11540
+                val larousse:Int=11640
+
+}
+
+fun imprmir(){
+
+    println("El Libro: $papelucho")
+
+
+}
